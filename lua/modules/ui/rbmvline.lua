9eec8916febb707d83c38e475721e9f225eb7fe0
robertmitchellv <robert.beaty.mitchell@gmail.com>
2022-11-23 09:31:57 -0800

    finished the diff section and lsp bubbles; working on right side

--- lua/modules/ui/rbmvline.lua
+++ lua/modules/ui/rbmvline.lua
@@ -19,33 +19,57 @@ end
 local colors = tokyo_colors.setup()
 local util = require("tokyonight.util")
 
--- --> custom colors for background bubbles
+-- </> custom colors for git info
+-- --> blend(fg, bg, alpha)
 colors.galaxyGit = {}
-colors.galaxyGit[1] = util.blend(colors.bg, colors.green, .4)
-colors.galaxyGit[2] = util.blend(colors.bg, colors.green, .8)
+colors.galaxyGit.icon_bg = util.blend(colors.purple, colors.bg, .8)
+colors.galaxyGit.icon_fg = util.blend(colors.fg, colors.galaxyGit.icon_bg, .8)
+colors.galaxyGit.branch_bg = util.blend(colors.purple, colors.bg, .5)
+colors.galaxyGit.branch_fg = util.blend(colors.fg, colors.galaxyGit.branch_bg, .8)
+colors.galaxyGit.add_bg = util.blend(colors.git.add, colors.bg, .5)
+colors.galaxyGit.add_fg = util.blend(colors.fg, colors.galaxyGit.add_bg, .8)
+colors.galaxyGit.change_bg = util.blend(colors.git.change, colors.bg, .5)
+colors.galaxyGit.change_fg = util.blend(colors.fg, colors.galaxyGit.change_bg, .8)
+colors.galaxyGit.delete_bg = util.blend(colors.git.delete, colors.bg, .5)
+colors.galaxyGit.delete_fg = util.blend(colors.fg, colors.galaxyGit.delete_bg, .8)
+
+-- </> custom colors for lsp info
+-- --> blend(fg, bg, alpha)
+colors.galaxyLsp = {}
+colors.galaxyLsp.icon_bg = util.blend(colors.comment, colors.bg, .8)
+colors.galaxyLsp.icon_fg = util.blend(colors.fg, colors.galaxyLsp.icon_bg, .8)
+colors.galaxyLsp.name_bg = util.blend(colors.comment, colors.bg, .4)
+colors.galaxyLsp.name_fg = util.blend(colors.fg, colors.galaxyLsp.name_bg, .8)
 
 -- --> vim mode colors
 local mode_color = {
-  ["!"] = colors.red,
-  [""] = colors.blue,
-  [""] = colors.orange,
-  ["r?"] = colors.cyan,
-  c = colors.magenta,
-  ce = colors.red,
-  cv = colors.red,
-  i = colors.green,
-  ic = colors.yellow,
-  n = colors.red,
-  no = colors.red,
-  r = colors.cyan,
-  R = colors.violet,
-  rm = colors.cyan,
-  Rv = colors.violet,
-  s = colors.orange,
-  S = colors.orange,
-  t = colors.red,
-  v = colors.blue,
-  V = colors.blue,
+  -- command
+  c = colors.yellow, -- command
+  ce = colors.yellow, -- ex
+  cv = colors.yellow, -- vim ex
+  ["!"] = colors.yellow, -- shell
+  -- insert
+  i = colors.green, -- insert
+  -- normal
+  n = colors.blue, -- normal
+  no = colors.blue, -- n-operator pending
+  -- replace
+  R = colors.red, -- replace
+  Rv = colors.red, -- v-replace
+  -- visual
+  v = colors.magenta, -- visual
+  V = colors.magenta, -- v-line
+  [""] = colors.magenta, -- v-block
+  -- select
+  s = colors.orange, -- select
+  S = colors.orange, -- s-line
+  [""] = colors.orange, -- s-block
+  -- other
+  ic = colors.teal, -- ?
+  r = colors.teal, -- prompt
+  rm = colors.teal, -- more
+  ["r?"] = colors.teal, -- confirm
+  t = colors.teal,
 }
 
 -- --> bubble separators
@@ -62,7 +86,7 @@ gls.left[index] = {
     provider = function()
       -- auto change color according the vim mode
       vim.api.nvim_command(
-        "hi GalaxyModeBarLeft guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg)
+        "hi GalaxyModeBarLeft guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg_statusline)
       return "▊ "
     end,
   },
@@ -74,17 +98,8 @@ gls.left[index] = {
     provider = function()
       -- auto change color according the vim mode
       vim.api.nvim_command(
-        "hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg)
+        "hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg_statusline)
       return "  "
-    end,
-  },
-}
-
-index = index + 1
-gls.left[index] = {
-  FirstSpacer = {
-    provider = function()
-      return " "
     end,
   },
 }
@@ -95,7 +110,8 @@ gls.left[index] = {
     provider = function()
       return separators.left
     end,
-    highlight = { colors.galaxyGit[1], colors.bg },
+    highlight = { colors.galaxyGit.icon_bg, colors.bg_statusline },
+    condition = condition.check_git_workspace,
   },
 }
 
@@ -105,9 +121,9 @@ gls.left[index] = {
     provider = function()
       return "    "
     end,
-    highlight = { colors.fg, colors.galaxyGit[1] },
+    highlight = { colors.galaxyGit.icon_fg, colors.galaxyGit.icon_bg },
     separator = separators.right,
-    separator_highlight = { colors.galaxyGit[1], colors.galaxyGit[2] },
+    separator_highlight = { colors.galaxyGit.icon_bg, colors.galaxyGit.branch_bg },
     condition = condition.check_git_workspace,
   },
 }
@@ -118,7 +134,8 @@ gls.left[index] = {
     provider = function()
       return "   "
     end,
-    highlight = { colors.fg, colors.galaxyGit[2] },
+    highlight = { colors.galaxyGit.branch_fg, colors.galaxyGit.branch_bg },
+    condition = condition.check_git_workspace,
   },
 }
 
@@ -126,9 +143,9 @@ index = index + 1
 gls.left[index] = {
   GitBranch = {
     provider = "GitBranch",
-    highlight = { colors.fg, colors.galaxyGit[2] },
+    highlight = { colors.galaxyGit.branch_fg, colors.galaxyGit.branch_bg },
     separator = separators.right,
-    separator_highlight = { colors.galaxyGit[2], colors.bg },
+    separator_highlight = { colors.galaxyGit.branch_bg, colors.galaxyGit.add_bg },
     condition = condition.check_git_workspace,
   },
 }
@@ -139,6 +156,7 @@ gls.left[index] = {
     provider = function()
       return " "
     end,
+    highlight = { colors.fg, colors.galaxyGit.add_bg },
   },
 }
 
@@ -147,8 +165,10 @@ gls.left[index] = {
   DiffAdd = {
     provider = "DiffAdd",
     icon = "  ",
-    --highlight = { colors.diff.add, colors.bg },
-    --condition = condition.check_git_workspace, 
+    highlight = { colors.galaxyGit.add_fg, colors.galaxyGit.add_bg },
+    separator = separators.right,
+    separator_highlight = { colors.galaxyGit.add_bg, colors.galaxyGit.change_bg },
+    condition = condition.check_git_workspace, 
   },
 }
 
@@ -157,8 +177,10 @@ gls.left[index] = {
   DiffModified = {
     provider = "DiffModified",
     icon = "  ",
-    --highlight = { colors.diff.change, colors.bg },
-    --condition = condition.check_git_workspace,
+    highlight = { colors.galaxyGit.change_fg, colors.galaxyGit.change_bg },
+    separator = separators.right,
+    separator_highlight = { colors.galaxyGit.change_bg, colors.galaxyGit.delete_bg },
+    condition = condition.check_git_workspace,
   },
 }
 
@@ -167,47 +189,31 @@ gls.left[index] = {
   DiffRemove = {
     provider = "DiffRemove",
     icon = "  ",
-    --highlight = { colors.diff.delete, colors.bg },
-    --condition = condition.check_git_workspace,
+    highlight = { colors.galaxyGit.delete_fg, colors.galaxyGit.delete_bg },
+    separator = separators.right,
+    separator_highlight = { colors.galaxyGit.delete_bg, colors.bg_statusline },
+    condition = condition.check_git_workspace,
   },
 }
-
---gls.left[12] = {
---  DiagnosticError = {
---    highlight = { colors.red, colors.bg },
---    icon = "  ",
---    provider = "DiagnosticError",
---  },
---}
-
---gls.left[13] = {
---  DiagnosticWarn = {
---    highlight = { colors.yellow, colors.bg },
---    icon = "  ",
---    provider = "DiagnosticWarn",
---  },
---}
-
---gls.left[14] = {
---  DiagnosticHint = {
---    highlight = { colors.cyan, colors.bg },
---    icon = "  ",
---    provider = "DiagnosticHint",
---  },
---}
-
---gls.left[15] = {
---  DiagnosticInfo = {
---    highlight = { colors.blue, colors.bg },
---    icon = "  ",
---    provider = "DiagnosticInfo",
---  },
---}
 
 -- --> mid
 index = 1
 gls.mid[index] = {
-  ShowLspClient = {
+  ShowLspBubbleLeft = {
+    provider = function()
+      return separators.left
+    end,
+    highlight = { colors.galaxyLsp.icon_bg, colors.bg_statusline },
+  },
+}
+
+index = index + 1
+gls.mid[index] = {
+  ShowLspIcon = {
+    provider = function()
+      return "  lsp  "
+    end,
+    highlight = { colors.galaxyLsp.icon_fg, colors.galaxyLsp.icon_bg },
     condition = function()
       local tbl = { ["dashboard"] = true, [""] = true }
       if tbl[vim.bo.filetype] then
@@ -215,14 +221,137 @@ gls.mid[index] = {
       end
       return true
     end,
-    highlight = { colors.yellow, colors.bg, "bold" },
-    icon = "   lsp   ",
+  },
+}
+
+-- in the sections earlier separator/separator_highlight worked well;
+-- with this section, it was moving them to places that didn't make sense
+-- so I added extra space/separator sections to make it work
+index = index + 1
+gls.mid[index] = {
+  ShowLspBubbleSeparator = {
+    provider = function()
+      return ""
+    end,
+    highlight = { colors.galaxyLsp.icon_fg, colors.galaxyLsp.icon_bg },
+    separator = separators.right,
+    separator_highlight = { colors.galaxyLsp.icon_bg, colors.galaxyLsp.name_bg },
+    condition = function()
+      local tbl = { ["dashboard"] = true, [""] = true }
+      if tbl[vim.bo.filetype] then
+        return false
+      end
+      return true
+    end,
+  },
+}
+
+index = index + 1
+gls.mid[index] = {
+  ShowLspNameStartSpacer = {
+    provider = function()
+      return " "
+    end,
+    highlight = { colors.galaxyLsp.name_fg, colors.galaxyLsp.name_bg },
+    condition = function()
+      local tbl = { ["dashboard"] = true, [""] = true }
+      if tbl[vim.bo.filetype] then
+        return false
+      end
+      return true
+    end,
+  },
+}
+
+index = index + 1
+gls.mid[index] = {
+  ShowLspClient = {
     provider = "GetLspClient",
+    highlight = { colors.galaxyLsp.name_fg, colors.galaxyLsp.name_bg },
+    condition = function()
+      local tbl = { ["dashboard"] = true, [""] = true }
+      if tbl[vim.bo.filetype] then
+        return false
+      end
+      return true
+    end,
+  },
+}
+
+index = index + 1
+gls.mid[index] = {
+  ShowLspNameEndSpacer = {
+    provider = function()
+      return " "
+    end,
+    highlight = { colors.galaxyLsp.name_fg, colors.galaxyLsp.name_bg },
+    condition = function()
+      local tbl = { ["dashboard"] = true, [""] = true }
+      if tbl[vim.bo.filetype] then
+        return false
+      end
+      return true
+    end,
+  },
+}
+
+index = index + 1
+gls.mid[index] = {
+  ShowLspBubbleRight = {
+    provider = function()
+      return ""
+    end,
+    highlight = { colors.galaxyLsp.icon_fg, colors.galaxyLsp.icon_bg },
+    separator = separators.right,
+    separator_highlight = { colors.galaxyLsp.name_bg, colors.bg_statusline },
+    condition = function()
+      local tbl = { ["dashboard"] = true, [""] = true }
+      if tbl[vim.bo.filetype] then
+        return false
+      end
+      return true
+    end,
   },
 }
 
 -- --> right
 index = 1
+gls.right[index] = {
+  DiagnosticError = {
+    highlight = { colors.red, colors.bg_statusline },
+    icon = "  ",
+    provider = "DiagnosticError",
+  },
+}
+
+index = index + 1
+gls.right[index] = {
+  DiagnosticWarn = {
+    highlight = { colors.yellow, colors.bg_statusline },
+    icon = "  ",
+    provider = "DiagnosticWarn",
+  },
+}
+
+index = index + 1
+gls.right[index] = {
+  DiagnosticHint = {
+    highlight = { colors.cyan, colors.bg_statusline },
+    icon = "  ",
+    provider = "DiagnosticHint",
+  },
+}
+
+index = index + 1
+gls.right[index] = {
+  DiagnosticInfo = {
+    highlight = { colors.blue, colors.bg },
+    icon = "  ",
+    provider = "DiagnosticInfo",
+  },
+}
+
+index = index + 1
 gls.right[index] = {
   FileEncode = {
     condition = condition.hide_in_width,
@@ -250,6 +379,7 @@ gls.right[index] = {
     provider = function()
       return " "
     end,
+    highlight = { colors.green, colors.bg_statusline },
   },
 }
 
@@ -259,7 +389,7 @@ gls.right[index] = {
     provider = function()
       -- auto change color according the vim mode
       vim.api.nvim_command(
-        "hi GalaxyModeBarRight guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg)
+        "hi GalaxyModeBarRight guifg=" .. mode_color[vim.fn.mode()] .. " guibg=" .. colors.bg_statusline)
       return " ▊"
     end,
   },
