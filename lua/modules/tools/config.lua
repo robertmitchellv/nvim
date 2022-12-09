8b00a3886e1c7ca03067b518692c9754d0a01ddc
robertmitchellv <robert.beaty.mitchell@gmail.com>
2022-12-09 15:35:29 -0800

    added rainbow parens via treesitter

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -91,6 +91,19 @@ function config.nvim_treesitter()
     highlight = {
       enable = true,
     },
+    rainbow = {
+      enable = true,
+      -- list of languages you want to disable the plugin for
+      -- disable = { "jsx", "cpp" },
+      -- highlight non-bracket delimiters like html tags, boolean or table: 
+      -- lang -> boolean
+      extended_mode = true,
+       -- Do not enable for files with more than n lines, int
+      max_file_lines = nil,
+      -- table of hex strings
+      -- colors = {},
+      -- termcolors = {} -- table of colour name strings
+    },
     textobjects = {
       select = {
         enable = true,
--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -27,6 +27,8 @@ plugin({
 
 plugin({ "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" })
 
+plugin({ "p00f/nvim-ts-rainbow", after = "nvim-treesitter" })
+
 plugin({
   "folke/trouble.nvim",
   config = conf.trouble,
