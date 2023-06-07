1885bbd90aac0607627e702e9344ee206ba3c18d
robertmitchellv <robert.beaty.mitchell@gmail.com>
2023-06-07 16:18:24 -0700

    added yarepl package; updated lazy

---
+++ lua/plugins/repl.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "milanglacier/yarepl.nvim",
+    config = function()
+      local yarepl = require("yarepl")
+
+      yarepl.setup({
+        buflisted = true,
+        scratch = true,
+        ft = "REPL",
+        metas = {
+          python = { cmd = "python", formatter = yarepl.formatter.trim_empty_lines },
+          ipython = { cmd = "ipython", formatter = yarepl.formatter.bracketed_pasting },
+          R = { cmd = "R", formatter = yarepl.formatter.trim_empty_lines },
+          radian = { cmd = "radian", formatter = yarepl.formatter.bracketed_pasting },
+          julia = { cmd = "julia", formatter = yarepl.formatter.bracketed_pasting },
+        },
+        close_on_exit = true,
+        scroll_to_bottom_after_sending = true,
+      })
+    end,
+  },
+}
