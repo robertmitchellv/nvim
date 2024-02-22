3b88828effd4dc4148ae4113a3e8a2587424402d
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-02-21 21:59:13 -0800

    adding some autoformatters; this should have flake8 baked in

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -12,4 +12,13 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "ruff_format" },
+      },
+    },
+  },
 }
