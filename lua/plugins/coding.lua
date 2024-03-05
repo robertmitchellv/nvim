a96c969387863151e82d432f9a9002c3179ca89b
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-03-04 21:44:37 -0800

    working on getting the python dap to work

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -13,11 +13,45 @@ return {
     },
   },
   {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+        { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class" },
+      },
+      config = function()
+        local path = require("mason-registry").get_package("debugpy"):get_install_path()
+        require("dap-python").setup(path .. "/venv/bin/python")
+      end,
+    },
+  },
+  {
     "stevearc/conform.nvim",
-    optional = true,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     opts = {
       formatters_by_ft = {
-        ["python"] = { "ruff_format" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        python = {
+          "reorder-python-imports",
+          "black",
+          "ruff_fix",
+          "ruff_format",
+        },
+        toml = { "taplo" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 1000,
       },
     },
   },
