be4fa42c5491e31e6472183dc4b248dca5d51574
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-10-15 12:43:16 -0700

    removing copilot from the setup

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,18 +1,5 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-  {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
