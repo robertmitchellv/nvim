da55b62ac7808b1cd9a59f8a7de7abc09481040d
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-12-18 14:54:02 -0800

    disable default dashboard for my custom one defined in ui.lua

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,8 +2,11 @@ return {
   -- i need to add this here so that lazyvim doesn't add its default
   -- welcome screen
   {
-    "goolord/alpha-nvim",
-    enabled = false,
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        enabled = false,
+      },
+    },
   },
-
 }
