3420aad9bc4fcfdaf984d8825512b5ce73ff932c
robertmitchellv <robert.beaty.mitchell@gmail.com>
2025-01-30 20:59:08 -0800

    adding the kulala lsp

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,6 +3,17 @@ return {
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
+    config = function()
+      require("conform").setup({
+        formatters = {
+          kulala = {
+            command = "kulala-fmt",
+            args = { "$FILENAME" },
+            stdin = false,
+          },
+        },
+      })
+    end,
     opts = {
       formatters_by_ft = {
         javascript = { "prettier" },
@@ -18,7 +29,9 @@ return {
           "ruff_format",
         },
         toml = { "taplo" },
+        http = { "kulala" },
       },
+      format_on_save = true,
     },
   },
   {
