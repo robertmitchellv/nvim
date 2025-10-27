070ef99024eae2e09c2a571a0a350c28ac3ad69d
robertmitchellv <robert@robertmitchellv.com>
2025-10-26 23:19:01 -0700

    add for wayland copypasta? not sure

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -58,11 +58,17 @@ cmd("hi link Keyword GitBlame")
 -- clipboard integration
 opt.clipboard = "unnamedplus"
 
--- jinja filetype detection
-vim.filetype.add({
-  extension = {
-    jinja = "jinja",
-    jinja2 = "jinja",
-    j2 = "jinja",
-  },
-})
+if vim.fn.executable("wl-copy") == 1 and vim.fn.executable("wl-paste") == 1 then
+  vim.g.clipboard = {
+    name = "wl-clipboard",
+    copy = {
+      ["+"] = "wl-copy --foreground --type text/plain",
+      ["*"] = "wl-copy --foreground --type text/plain",
+    },
+    paste = {
+      ["+"] = "wl-paste --no-newline",
+      ["*"] = "wl-paste --no-newline",
+    },
+    cache_enabled = 0,
+  }
+end
