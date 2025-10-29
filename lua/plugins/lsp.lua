4c4ef861e699df007380a42e136e7fa8c89497a6
robertmitchellv <robert@robertmitchellv.com>
2025-10-28 21:09:47 -0700

    add best new tiny inlay diagnostic package :tada:

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -165,4 +165,14 @@ return {
       },
     },
   },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = { diagnostics = { virtual_text = false } },
+  },
 }
