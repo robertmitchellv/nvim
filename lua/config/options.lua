f2ad501c87e7755af4ff43617175a3d123c24691
robertmitchellv <robert@robertmitchellv.com>
2025-10-28 21:09:15 -0700

    fix copy/paste on wayland

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -57,18 +57,3 @@ cmd("hi link Keyword GitBlame")
 
 -- clipboard integration
 opt.clipboard = "unnamedplus"
-
-if vim.fn.executable("wl-copy") == 1 and vim.fn.executable("wl-paste") == 1 then
-  vim.g.clipboard = {
-    name = "wl-clipboard",
-    copy = {
-      ["+"] = "wl-copy --foreground --type text/plain",
-      ["*"] = "wl-copy --foreground --type text/plain",
-    },
-    paste = {
-      ["+"] = "wl-paste --no-newline",
-      ["*"] = "wl-paste --no-newline",
-    },
-    cache_enabled = 0,
-  }
-end
